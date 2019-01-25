6436a7f4d2f629765f695150a59684ee2c2d7e28
Murali Medisetty <mmedisetty@uber.com>
2019-01-24 20:30:49 -0800

    Initializes new extension

---
+++ background.js
@@ -0,0 +1,23 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log('[set] color: ', '#3aa757');
+  });
+
+  chrome.storage.sync.get(['color'], function(data) {
+    console.log('[get] color: ', data.color);
+  });
+
+
+  chrome.history.search({text: '', maxResults: 1000}, function(data) {
+    console.log('Got your browser history! ');
+    data.forEach(function(page) {
+        console.log(page.url);
+    });
+});
+});
